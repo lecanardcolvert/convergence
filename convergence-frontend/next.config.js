f0c68eb16bdd59df78b277958eabfbf5332e427b
lecanardcolvert <59675854+lecanardcolvert@users.noreply.github.com>
2023-02-24 11:41:24 -0500

    update project config

---
+++ convergence-frontend/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5140/api/:path*",
+      },
+    ];
+  },
+};
