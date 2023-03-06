cc73c3d76ce1d65fe054ec6a40c0b486271691fc
lecanardcolvert <59675854+lecanardcolvert@users.noreply.github.com>
2023-03-06 14:04:04 -0500

    fix: nextjs rewrites to /api backend

--- convergence-frontend/next.config.js
+++ convergence-frontend/next.config.js
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    i18n: {
-        locales: ["fr"],
-        defaultLocale: "fr",
-    },
-}
-
 module.exports = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5140/api/:path*",
+        destination: "http://localhost:5140/api/:path*"
       },
     ];
   },
 };
-
-module.exports = nextConfig;
